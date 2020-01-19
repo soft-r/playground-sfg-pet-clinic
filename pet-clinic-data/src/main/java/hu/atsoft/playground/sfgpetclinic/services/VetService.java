efd315d0328b0905acaf7f53e40376616c8da84e
dp <hudomokp@gmail.com>
2020-01-19 19:52:10 +0100

    Added service interface for owner, pet and vet

---
+++ pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package hu.atsoft.playground.sfgpetclinic.services;
+
+import hu.atsoft.playground.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
