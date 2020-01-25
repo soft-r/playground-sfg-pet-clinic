d733122411cfa2bd2f8a55af9510ba70228e848d
dp <hudomokp@gmail.com>
2020-01-25 11:20:04 +0100

    Factored out common service methods into CrudService

--- pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/services/VetService.java
@@ -2,10 +2,5 @@ package hu.atsoft.playground.sfgpetclinic.services;
 
 import hu.atsoft.playground.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
